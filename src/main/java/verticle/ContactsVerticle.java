a77c79baaaa4f03d5a35495abb1d5d3cc05785d4
CatherineNezhumira <katerinananan@gmail.com>
2017-04-14 12:08:53 +0300

    delete comments

--- src/main/java/verticle/ContactsVerticle.java
+++ src/main/java/verticle/ContactsVerticle.java
@@ -22,31 +22,6 @@ public class ContactsVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> fut) {
-//        router.route("/").handler(routingContext -> {
-//            HttpServerResponse response = routingContext.response();
-//            response
-//                    .putHeader("content-type", "text/html")
-//                    .end("<h1>Hello from my first Vert.x 3 application</h1>");
-//        });
-//
-//        // Create the HTTP server and pass the "accept" method to the request handler.
-//        vertx
-//                .createHttpServer()
-//                .requestHandler(router::accept)
-//                .listen(
-//                        // Retrieve the port from the configuration,
-//                        // default to 8080.
-//                        config().getInteger("http.port", 8080),
-//                        result -> {
-//                            if (result.succeeded()) {
-//                                fut.complete();
-//                            } else {
-//                                fut.fail(result.cause());
-//                            }
-//                        }
-//                );
-//    }
-
         Router router = Router.router(vertx);
         router.route("/api/contacts*").handler(BodyHandler.create());
         router.get("/api/contacts").handler(this::getAll);
